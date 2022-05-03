53f10985b6b61413136c1db4bed522df3a66d878
natabask <natabask@yandex.ru>
2022-05-03 08:57:07 +0300

    initial commit - tests passed

---
+++ src/test/java/ApiPostTest.java
@@ -0,0 +1,25 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class ApiPostTest {
+
+    @Test
+    void shouldCheckIfBodyContainsUsername() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("username: мария иванова") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", containsString("username"));
+    }
+}
